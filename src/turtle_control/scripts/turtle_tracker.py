18de0d9388e73e902909e5d05b3c2cd09cc3aa3e
dandyidk <45296953+dandyidk@users.noreply.github.com>
2024-08-26 21:25:06 +0300

    Update turtle_tracker.py

--- src/turtle_control/scripts/turtle_tracker.py
+++ src/turtle_control/scripts/turtle_tracker.py
@@ -4,7 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import sys
-
+#hello
 class TurtleTracker:
     def __init__(self):
 
@@ -16,29 +16,19 @@ class TurtleTracker:
                                        'y':2} #this variable holds all the positions of all turtles
             #simply type in self.turtles.tracker[turtlename]['x' or 'y']
         self.subscribers = {} #for subscribing all turtles
-        self.subscribers = {}
         for i in arg[1:]:
             self.subscribers[i] = rospy.Subscriber(f'{i}/pose',Pose,self.callback,callback_args= i)
 
         self.pose = Pose()
-<<<<<<< HEAD
-        for i in arg[1:]:
-            print(self.turtles_tracker[i]['x'])
-=======
->>>>>>> Created a node that tracks coordinates
     def callback(self,data,name):
         self.turtles_tracker[name] = {
             'x':data.x,
             'y':data.y
         }
-<<<<<<< HEAD
+        print(self.turtles_tracker[name]['x']) # a little test to see all coordinates
         
-=======
-        print(self.turtles_tracker[name]['x']) # a little test to see all x coordinates
->>>>>>> Created a node that tracks coordinates
     def run(self):
         rospy.spin()
 if __name__ == '__main__':
     tracker = TurtleTracker()
     tracker.run()
-
