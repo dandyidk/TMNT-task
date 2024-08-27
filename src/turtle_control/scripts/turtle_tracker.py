e620d9b22ec64fa8abacaeb1ed18db54d40795c0
dandyidk <45296953+dandyidk@users.noreply.github.com>
2024-08-27 19:57:00 +0300

    modified:   src/turtle_control/scripts/turtle_tracker.py

--- src/turtle_control/scripts/turtle_tracker.py
+++ src/turtle_control/scripts/turtle_tracker.py
@@ -35,13 +35,15 @@ class TurtleTracker:
     def attack(self,data):
         name = data.data
         print(name)
-        self.turtles_tracker[name]['attack'] -=1
+        self.turtles_tracker[name] ={'attack':1}
         print(2)
         for i in self.turtles_tracker:
             if i == name:
                 continue
             else:
-                hypo = math.sqrt(pow(self.turtles_tracker[i]['x'],2)+pow(self.turtles_tracker[i]['y'],2))
+                x =abs(self.turtles_tracker[i]['x']-self.turtles_tracker[name]['x'])
+                y =abs(self.turtles_tracker[i]['y']-self.turtles_tracker[name]['y'])
+                hypo = math.sqrt(pow(x,2)+pow(y,2))
                 if RADIUS<hypo:
                     self.turtles_tracker[i]['health'] -= 50
                     print(self.turtles_tracker[i]['health'])
